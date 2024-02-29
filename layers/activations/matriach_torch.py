e22524daf301dc338003b4d1c9323dcc65465ad1
stolym <piikxs@gmail.com>
2024-02-29 11:45:49 +0100

    :ambulance: fix: improvement Matriach(beta).

--- layers/activations/matriach_torch.py
+++ layers/activations/matriach_torch.py
@@ -5,11 +5,12 @@ class Matriach(nn.Module):
     # Homemade
     # Fusion/Mix between the performance of Gish and Logish
 
-    def __init__(self, alpha: float = 1.0, beta: float = 1.0, theta: float = 1, *args, **kwargs) -> None:
+    def __init__(self, alpha: float = 1.0, beta: float = 1.0, theta: float = 1.0, meta: float = 1.0, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.alpha = alpha
         self.beta = beta
         self.theta = theta
+        self.meta = meta
 
     def forward(self, x):
-        return x * self.alpha * (1/(self.theta+torch.log(1+torch.exp(-x*self.beta))))
+        return x * self.alpha * (1/(self.theta*1+torch.exp(-x*self.beta)*torch.log(2-torch.exp(-torch.exp(-x*self.meta)))))
